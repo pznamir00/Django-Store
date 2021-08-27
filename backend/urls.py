e5d6064a2f467b5c1b3e9b002ec83cb59970d34d
pznamir00 <pznamir00@gmail.com>
2021-08-27 11:52:20 +0200

    Add media dir

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from django.contrib import admin
 from .router import router
 from app.views import ProductAPIView, ProductDetailAPIView, UserDetailAPIView, CartAPIView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('api/auth/', include('rest_auth.urls')),
@@ -11,4 +13,4 @@ urlpatterns = [
     path('api/products/<slug:slug>/', ProductDetailAPIView.as_view()),
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
